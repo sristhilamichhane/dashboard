ff276cf7327d118d5dd2af24bdaaa53673cd8e23
Srishtilamichhane <59720998+Srishtilamichhane@users.noreply.github.com>
2022-05-24 08:20:22 +0545

    comitted

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,27 @@
+import React, { createContext, useContext, useState } from "react";
+
+const StateContext = createContext();
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
+export const ContextProvider = ({ children }) => {
+  const [activeMenu, setActiveMenu] = useState(true);
+
+  const [isClicked, setIsClicked] = useState(initialState);
+
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true });
+  };
+
+  return (
+    <StateContext.Provider
+      value={{ activeMenu, setActiveMenu, isClicked, setIsClicked }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+export const useStateContext = () => useContext(StateContext);
